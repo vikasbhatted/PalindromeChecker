b88803468b673242bf8cb0c126ac64e49d54ed12
Vikas Bhatted <vikasbhatted@gmail.com>
2026-04-01 09:04:56 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,21 @@
 public class PalindromeCheckerApp {
+
+    /**
+     * Application entry point for UC2.
+     *
+     * @param args Command-line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System!");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialized successfully.");
+        String input = "madam";
+        boolean result = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                result = false;
+                break;
+            }
+        }
+
+        System.out.println("Is it a Palindrome?: " + result);
     }
 
 }
